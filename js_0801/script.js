d12cee9869f14da1cbf237a334031de9c12164ee
kim5537 <fudgml778@naver.com>
2024-08-01 22:33:36 +0900

    0801

---
+++ js_0801/script.js
@@ -0,0 +1,34 @@
+// function (매개변수) {실행문}
+
+// function calcSum(a) {
+//   prompt(`%{a}`);
+// }
+
+// calcSum("매게변수를 밖으로 가져올 수 있다.")
+
+//-----------------
+
+// const calcSum = function(){}
+// //자료형에 함수 넣기 가능
+
+// function calcSum() {
+//   let sum = 0; //값을 넣을 바구니 제작
+//   for (let i = 1; 1 <= 10; i++) {
+//     sum += i;
+//   }
+//   console.log(`${sum}`);
+// }
+
+// calcSum();
+
+//  익명
+
+// const calcSum = function () {
+//   let sum = 0;
+//   for (let i = 1; 1 <= 10; i++) {
+//     sum += i;
+//   }
+//   console.log(`1부터 10가지 더하면 ${sum}`);
+// };
+
+//=> 함수자체를 변수로 지정하여 해당 변수가 함수가 되었다. 곧 함수=변수
